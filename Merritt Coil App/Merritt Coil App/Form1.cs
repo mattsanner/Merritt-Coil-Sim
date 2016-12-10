634ca56d519525f9776d0c4c7e82a5079649ebf1
mattsanner <mwsanner@ksu.edu>
2016-12-10 00:33:44 -0600

    Added equation bits to GUI program

--- Merritt Coil App/Merritt Coil App/Form1.cs
+++ Merritt Coil App/Merritt Coil App/Form1.cs
@@ -12,6 +12,11 @@ namespace Merritt_Coil_App
 {
     public partial class Form1 : Form
     {
+        double width = 1;
+        double length = 1;
+        double muNot = 4 * Math.PI * Math.Pow(10, 7);
+        double unitCurrent = 1;
+
         public Form1()
         {
             InitializeComponent();
@@ -35,13 +40,39 @@ namespace Merritt_Coil_App
 
             double[] answers = BFieldEquation(ax, ay, az);
 
-            bFieldText.Text = "B = " + answers[0] + "âx + " + answers[1] + "ây + " + answers[2] + "âz";
+            bFieldText.Text = "B = " + answers[0].ToString("0.##") + "âx + " + answers[1].ToString("0.##") + "ây + " + answers[2].ToString("0.##") + "âz";
         }
 
         private double[] BFieldEquation(double x, double y, double z)
         {
             //enter equation stuff here
             double[] retVals = { x, y, z };
+
+            double c1 = x + length;
+            double c2 = x - length;
+            double c3 = x - length;
+            double c4 = x + length;
+
+            double d1 = y + width;
+            double d2 = y + width;
+            double d3 = y - width;
+            double d4 = y - width;
+
+            double r1 = Math.Sqrt(Math.Pow((x + length), 2) + Math.Pow((y + width), 2) + Math.Pow(z, 2));
+            double r2 = Math.Sqrt(Math.Pow((x - length), 2) + Math.Pow((y + width), 2) + Math.Pow(z, 2));
+            double r3 = Math.Sqrt(Math.Pow((x - length), 2) + Math.Pow((y - width), 2) + Math.Pow(z, 2));
+            double r4 = Math.Sqrt(Math.Pow((x + length), 2) + Math.Pow((y - width), 2) + Math.Pow(z, 2));
+
+            double Bx = z*((1/(r1*(r1+d1))) - (1/(r2*(r2+d2))) + (1/(r3*(r3+d3))) - (1/(r4*(r4+d4))));
+            double By = z*((1/(r1*(r1+c1))) - (1/(r2*(r2+c2))) + (1/(r3*(r3+c3))) - (1/(r4*(r4+c4))));
+            double Bz = (-1) * ((c1/(r1*(r1+d1))) + (d1/(r1*(r1+c1)))) + ((c2/(r2*(r2+d2))) + (d2/(r2*(r2+c2)))) - ((c3/(r3*(r3+d3))) + (d3/(r3*(r3+c3)))) + ((c4/(r4*(r4+d4))) + (d4/(r4*(r4+c4))));
+            Bx = Bx * (muNot * unitCurrent) / (4 * Math.PI);
+            By = By * (muNot * unitCurrent) / (4 * Math.PI);
+            Bz = Bz * (muNot * unitCurrent) / (4 * Math.PI);
+
+            retVals[0] = Bx;
+            retVals[1] = By;
+            retVals[2] = Bz;
 
             return retVals;
         }
